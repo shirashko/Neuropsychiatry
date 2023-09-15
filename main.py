1873506413d1d6dedfda82834f4902f30ba1474f
Shir Rashkovits <shirashko@gmail.com>
2023-09-15 17:41:57 +0300

    extract some code into functions

--- main.py
+++ main.py
@@ -8,34 +8,12 @@ from sklearn.decomposition import PCA
 
 
 def main():
-    # loading data and preprocessing
-    data_raw = pd.read_csv('data-final.csv', sep='\t')
-    data = data_raw.copy()
-    pd.options.display.max_columns = 150
-    # Some columns of the dataset are dropped, and any rows containing NaN values are also dropped.
-    data.drop(data.columns[50:107], axis=1, inplace=True)
-    data.drop(data.columns[51:], axis=1, inplace=True)
-    data.dropna(inplace=True)
-    data = data.drop('country', axis=1)
-    data = data[(data > 1).any(axis=1)]
-    data = data[(data < 5).any(axis=1)]
+    data = load_data_and_prepare_for_analysis()
 
-    CON = data.iloc[:, 30:40].mean(axis=1)  # compute for showing correlation in big 5 later
+    CON = compute_conciencious_mean(data) # for computing correlation of traits in Big Five model later
 
-    # process data to fit the 6 factor model we suggest
-    # Rename the columns: define new column names for our model for Ambitious and Methodical factors.
-    new_column_names = [column for column in data]
-    new_column_names[30] = "AMB1"
-    new_column_names[31] = "MET1"
-    new_column_names[32] = "MET2"
-    new_column_names[33] = "MET3"
-    new_column_names[34] = "AMB2"
-    new_column_names[35] = "MET4"
-    new_column_names[36] = "MET5"
-    new_column_names[37] = "MET6"
-    new_column_names[38] = "AMB3"
-    new_column_names[39] = "MET7"
-    data.columns = new_column_names
+    # change conscientious columns into Ambitious and Methodical factors, according to the six factor model we suggest.
+    adjust_columns_to_six_factor_model(data)
 
     # Define trait groups and their corresponding questions
     trait_groups = {
@@ -53,10 +31,8 @@ def main():
         trait_data[trait] = data[questions].mean(axis=1)
 
 
-    # Apply MinMax scaling to the trait data features by scaling each feature to a given range (0 to 1 in this case).
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    scaled_trait_data = scaler.fit_transform(trait_data)
-    scaled_trait_data = pd.DataFrame(scaled_trait_data, columns=trait_data.columns)
+    # Apply scaling to the trait data features by scaling each feature to [0,1] range.
+    scaled_trait_data = scale_data(trait_data)
 
     # Perform k-means clustering - define 6 clusters and fit the model
     kmeans = KMeans(n_clusters=6, random_state=0, n_init=10)
@@ -68,35 +44,14 @@ def main():
     # Calculate the mean trait values for each cluster
     mean_trait_clusters = scaled_trait_data.groupby('Clusters').mean()
 
-    # Create subplots
-    fig, axs = plt.subplots(1, 6, figsize=(22, 5), sharey=True)
-    for cluster_id in range(1, 7):  
-        ax = axs[cluster_id - 1]
-        cluster_data = mean_trait_clusters.loc[cluster_id]
-        cluster_data.plot(kind='bar', ax=ax, color='green', alpha=0.2)
-        cluster_data.plot(kind='line', ax=ax, color='red')
-        ax.set_title('Cluster ' + str(cluster_id))
-        ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
-        ax.set_ylim(0, 1)  # Adjust y-axis limits
-        ax.set_ylabel('Mean Trait Value')
+    display_cluster_mean_traits_plot(mean_trait_clusters)
 
-    plt.tight_layout()  # Adjust the layout
-    plt.show()
-
-    # Apply PCA on the clusters
-    pca = PCA(n_components=2)
-    pca_fit = pca.fit_transform(trait_data)
-    pca_data = pd.DataFrame(data=pca_fit, columns=['PCA1', 'PCA2'])
-
-    # Add adjusted cluster labels to PCA data
-    pca_data['Clusters'] = cluster_labels
+    # Apply PCA on the clusters, reduction to two dimension
+    pca_data = pca_for_dimension_reduction(cluster_labels, trait_data)
 
     # Visualization
-    plt.figure(figsize=(10, 10))
-    sns.scatterplot(data=pca_data, x='PCA1', y='PCA2', hue='Clusters', palette='Set2', alpha=0.8)
-    plt.title('Personality Clusters after PCA')
-    plt.show()
-    
+    display_pca_result(pca_data)
+
     # check correlation
 
     # Calculate the correlation matrix for the 6 factors model
@@ -104,10 +59,7 @@ def main():
     correlation_matrix = trait_data[factors].corr()
 
     # Visualize the correlation matrix using a heatmap
-    plt.figure(figsize=(10, 10))
-    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', vmin=-1, vmax=1)
-    plt.title('Correlation Matrix of Personality Factors')
-    plt.show()
+    display_correlation(correlation_matrix, 'Correlation Matrix of Personality Factors')
 
     # Compute the correlation matrix for factors with original conscientiousness (CON) - for the big 5 model
     trait_data_big_five = trait_data.drop(['MET', 'AMB'], axis=1)
@@ -116,10 +68,88 @@ def main():
     correlation_matrix_with_con = trait_data_big_five[factors_with_con].corr()
 
     # Visualize the correlation matrix using a heatmap for original conscientiousness (CON)
+    display_correlation(correlation_matrix_with_con, 'Correlation Matrix of Personality Factors with Original '
+                                                     'Conscientiousness')
+
+
+def pca_for_dimension_reduction(cluster_labels, trait_data):
+    # Apply PCA on the clusters, reduction to two dimension
+    pca = PCA(n_components=2)
+    pca_fit = pca.fit_transform(trait_data)
+    pca_data = pd.DataFrame(data=pca_fit, columns=['PCA1', 'PCA2'])
+    # Add adjusted cluster labels to PCA data
+    pca_data['Clusters'] = cluster_labels
+    return pca_data
+
+
+def display_correlation(correlation_matrix, title):
     plt.figure(figsize=(10, 10))
-    sns.heatmap(correlation_matrix_with_con, annot=True, cmap='coolwarm', vmin=-1, vmax=1)
-    plt.title('Correlation Matrix of Personality Factors with Original Conscientiousness')
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', vmin=-1, vmax=1)
+    plt.title(title)
     plt.show()
+
+
+def scale_data(trait_data):
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_trait_data = scaler.fit_transform(trait_data)
+    scaled_trait_data = pd.DataFrame(scaled_trait_data, columns=trait_data.columns)
+    return scaled_trait_data
+
+
+def display_pca_result(pca_data):
+    plt.figure(figsize=(10, 10))
+    sns.scatterplot(data=pca_data, x='PCA1', y='PCA2', hue='Clusters', palette='Set2', alpha=0.8)
+    plt.title('Personality Clusters after PCA')
+    plt.show()
+
+
+def display_cluster_mean_traits_plot(mean_trait_clusters):
+    # Create subplots
+    fig, axs = plt.subplots(1, 6, figsize=(22, 5), sharey=True)
+    for cluster_id in range(1, 7):
+        ax = axs[cluster_id - 1]
+        cluster_data = mean_trait_clusters.loc[cluster_id]
+        cluster_data.plot(kind='bar', ax=ax, color='green', alpha=0.2)
+        cluster_data.plot(kind='line', ax=ax, color='red')
+        ax.set_title('Cluster ' + str(cluster_id))
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
+        ax.set_ylim(0, 1)  # Adjust y-axis limits
+        ax.set_ylabel('Mean Trait Value')
+    plt.tight_layout()  # Adjust the layout
+    plt.show()
+
+
+def adjust_columns_to_six_factor_model(data):
+    new_column_names = [column for column in data]
+    new_column_names[30] = "AMB1"
+    new_column_names[31] = "MET1"
+    new_column_names[32] = "MET2"
+    new_column_names[33] = "MET3"
+    new_column_names[34] = "AMB2"
+    new_column_names[35] = "MET4"
+    new_column_names[36] = "MET5"
+    new_column_names[37] = "MET6"
+    new_column_names[38] = "AMB3"
+    new_column_names[39] = "MET7"
+    data.columns = new_column_names
+
+
+def compute_conciencious_mean(data):
+    return data.iloc[:, 30:40].mean(axis=1)  # compute for showing correlation in big 5 later
+
+def load_data_and_prepare_for_analysis():
+    # loading data and preprocessing
+    data_raw = pd.read_csv('data-final.csv', sep='\t')
+    data = data_raw.copy()
+    pd.options.display.max_columns = 150
+    # Some columns of the dataset are dropped, and any rows containing NaN values are also dropped.
+    data.drop(data.columns[50:107], axis=1, inplace=True)
+    data.drop(data.columns[51:], axis=1, inplace=True)
+    data.dropna(inplace=True)
+    data = data.drop('country', axis=1)
+    data = data[(data > 1).any(axis=1)]
+    data = data[(data < 5).any(axis=1)]
+    return data
 
 
 if __name__ == "__main__":
